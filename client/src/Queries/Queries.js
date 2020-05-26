9206d3cee2daf448b64885c0cde32f1bd03c94f1
Nyra Dev <37726406+akinwol@users.noreply.github.com>
2020-05-25 23:27:34 -0400

    updated test

--- client/src/Queries/Queries.js
+++ client/src/Queries/Queries.js
@@ -18,6 +18,8 @@ const getBooksQuery = gql`
 }
 `;
 const addBookMutation = gql`
+# the stuff in the () are the variables you want to pass to your mutation - these are query variables 
+# $ this sign lets you say that this is a query variable
 mutation($name:String!,$genre:String!, $authorId:ID!){
     addBook(name:$name, genre:$genre, authorId:$authorId){
         name
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,7 +11,7 @@ const {
     GraphQLID,
     GraphQLInt,
     GraphQLList,
-    GraphQLNonNull
+    GraphQLNonNull // this is to let us know that a certain field is required
 } = graphql;
 
 // dummy data
@@ -31,14 +31,15 @@ const {
 //     { name: 'Terry Patchett', genre: 66, id: '3' },
 // ];
 const BookType = new GraphQLObjectType({ //this function defines what the object is about
-    name: 'Book',
-    fields: () => ({ // this function returns the object below
+    name: 'Book', // the name of the object
+    fields: () => ({ // this function returns the object below - must need to be a function because different types can reference each other
         id: { type: GraphQLID }, //can use Graphql string here too but id will let you query with both string or not
         name: { type: GraphQLString },
         genre: { type: GraphQLString },
         author: {
             type: AuthorType,
             resolve(parent, args) {
+                //resolve function here does the same thing it does in the root queries below 
                 //when you have nested information you already have the parent data which is the book 
                 //return _.find(authors, { id: parent.authorId }) //finding an author with the author id of the parent 
                 return Author.findById(parent.authorId)
@@ -69,12 +70,14 @@ const RootQuery = new GraphQLObjectType({
     // rootqueries are how you initially jump into the graph to grab data
     name: 'RootQueryType',
     fields: { //don't need to wrap in a function here because order doesn't matter
-        book: {
-            type: BookType, //this param has been defined above 
+        book: { //each field is what you  enter from, so you have book - name you enter from 
+            type: BookType, //this param has been defined above - type of data you want to Query
             args: { id: { type: GraphQLID } },
             //when someone queries book type, they need to pass some arguments, this lets user pass this id
             resolve(parent, args) { //args are what are passed above 
+                //parent is more for relationsships 
                 // code to get data from db / other source
+                // args lets you access the arguments that have been passed to the query 
                 //return _.find(books, { id: args.id });
                 return Book.findById(args.id)
             }
@@ -113,15 +116,15 @@ const Mutation = new GraphQLObjectType({
         addAuthor: {
             type: AuthorType,
             args: {
-                name: { type: new GraphQLNonNull(GraphQLString) },
+                name: { type: new GraphQLNonNull(GraphQLString) }, //adding nonNull so that you can't omit passing a name 
                 age: {type: GraphQLInt}
             },
             resolve(parent, args) {
-                let author = new Author({ //creating new instance of the mongoose Author
+                let author = new Author({ //creating new instance of the mongoose Author from the model
                     name: args.name,
                     age: args.age
                 }) 
-                return author.save()
+                return author.save() //returning this gives you back what you just saved 
             }
         },
         addBook: {
@@ -144,7 +147,7 @@ const Mutation = new GraphQLObjectType({
 })
 
 module.exports = new GraphQLSchema({
-    query: RootQuery,
+    query: RootQuery, //this is the query we have defined 
     mutation:Mutation 
 });
 
@@ -170,6 +173,7 @@ module.exports = new GraphQLSchema({
 
 // mutation {
 //     addBook(name: "Finale II", genre: "Fantasy", authorId: "5ea58884b1e61136183ce8f9"){
+    // below is the data you want to recieve back when you create the data
 //         name
 //         genre
 //     }
